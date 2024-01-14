bcf957dd8cb46dcb12750af2ea21084a697a549f
Andrii Kotliar <andrii.ktlr@gmail.com>
2024-01-14 19:40:53 +0200

    add articles

---
+++ src/content/config.ts
@@ -0,0 +1,36 @@
+import { PageId } from '@/common';
+import { defineCollection, z } from 'astro:content';
+
+const articleSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  date: z.string().or(z.date()),
+});
+
+const lessons = defineCollection({
+  type: 'content',
+  schema: articleSchema,
+});
+
+const figures = defineCollection({
+  type: 'content',
+  schema: articleSchema,
+});
+
+const news = defineCollection({
+  type: 'content',
+  schema: articleSchema,
+});
+const stories = defineCollection({
+  type: 'content',
+  schema: articleSchema,
+});
+
+const collections = {
+  [PageId.LESSONS]: lessons,
+  [PageId.FIGURES]: figures,
+  [PageId.NEWS]: news,
+  [PageId.STORIES]: stories,
+};
+
+export { collections };
