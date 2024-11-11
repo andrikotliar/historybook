f8817ca9d7246c95c5e090faed700d561c15bc9e
Andrii Kotliar <andrii.ktlr@outlook.com>
2024-11-11 18:28:49 +0200

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import { metadataConfig } from './src/configs/metadata';
 
-export default defineConfig({});
+export default defineConfig({
+  site: metadataConfig.url,
+  integrations: [sitemap()],
+});
